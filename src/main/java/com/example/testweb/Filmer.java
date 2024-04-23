e544c25f5c37704e67c60535c379bcac9ee48dd8
liban0610 <115471778+liban0610@users.noreply.github.com>
2024-04-23 15:16:33 +0200

    oblig 3 d1700

---
+++ src/main/java/com/example/testweb/Filmer.java
@@ -0,0 +1,23 @@
+package com.example.testweb;
+
+public class Filmer {
+    private String film;
+
+    public Filmer( String film ){
+
+        this.film=film;
+    }
+
+    public Filmer() {
+
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+}
+
